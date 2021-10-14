f01d993400caa682088faf2052bc191d939bcf59
Jan Wijffels <jwijffels@bnosac.be>
2021-10-14 12:47:30 +0200

    namespace

--- R/pkg.R
+++ R/pkg.R
@@ -1,5 +1,5 @@
 #' @importFrom data.table rbindlist setnames setDF dcast.data.table
-#' @importFrom xml2 read_xml xml_children xml_name as_list
+#' @importFrom xml2 read_xml xml_children xml_name as_list xml_attr
 #' @importFrom magick geometry_area image_read image_crop image_append image_border
 #' @importFrom udpipe txt_collapse txt_recode
 #' @importFrom httr GET content
