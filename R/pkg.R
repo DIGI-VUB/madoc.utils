06ec94f0801a1b03d62a099cb8df2a2694393dc5
Jan Wijffels <jwijffels@bnosac.be>
2021-10-20 10:49:39 +0200

    r cmd check

--- R/pkg.R
+++ R/pkg.R
@@ -1,10 +1,11 @@
 #' @importFrom data.table rbindlist setnames setDF dcast.data.table
 #' @importFrom xml2 read_xml xml_children xml_name as_list xml_attr
-#' @importFrom magick geometry_area image_read image_crop image_append image_border image_draw
+#' @importFrom magick geometry_area image_read image_crop image_append image_border image_draw image_info image_resize
 #' @importFrom udpipe txt_collapse txt_recode
 #' @importFrom httr GET content
 #' @importFrom jsonlite fromJSON
 #' @importFrom stats setNames na.exclude lm predict
 #' @importFrom graphics lines 
 #' @importFrom grDevices dev.off
+#' @importFrom utils head tail
 NULL
