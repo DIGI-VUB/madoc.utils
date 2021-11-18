0a8f0ac31ed41e9ff9b98e31bd089a1d3bda7d21
Jan Wijffels <jwijffels@bnosac.be>
2021-11-18 09:15:45 +0100

    add as.data.table in namespace

--- R/pkg.R
+++ R/pkg.R
@@ -1,4 +1,4 @@
-#' @importFrom data.table rbindlist setnames setDF dcast.data.table
+#' @importFrom data.table rbindlist setnames setDF dcast.data.table as.data.table
 #' @importFrom xml2 read_xml xml_children xml_name as_list xml_attr
 #' @importFrom magick geometry_area image_read image_crop image_append image_border image_draw image_info image_resize
 #' @importFrom udpipe txt_collapse txt_recode
